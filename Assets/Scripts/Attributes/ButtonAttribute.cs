2a737851bd2d2ffffb074543d649c67bbf7cbaa5
ycarowr <ywrycaro@gmail.com>
2019-03-02 12:16:15 +0200

    Add ButtonAttribute for methods

---
+++ Assets/Scripts/Attributes/ButtonAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+/// <summary>
+/// This attribute tells the Editor to draw a for a Method.
+/// Since is using reflection it works for private and protected methods too.
+/// Ref: https://github.com/dbrizov/NaughtyAttributes
+/// </summary>
+[AttributeUsage(AttributeTargets.Method)]
+public class ButtonAttribute : Attribute
+{
+    public string Label { get; }
+
+    public ButtonAttribute(string label) : base()
+    {
+        Label = label;
+    }
+
+    public ButtonAttribute() : base()
+    {
+
+    }
+}

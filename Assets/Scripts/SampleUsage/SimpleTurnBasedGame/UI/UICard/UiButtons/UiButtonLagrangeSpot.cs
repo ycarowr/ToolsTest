499249e844da48476b02febeed1a8655d98af0cc
ycarowr <ywrycaro@gmail.com>
2019-03-10 18:15:52 +0200

    Add Adjustments to Test Card scene

---
+++ Assets/Scripts/SampleUsage/SimpleTurnBasedGame/UI/UICard/UiButtons/UiButtonLagrangeSpot.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiButtonLagrangeSpot : MonoBehaviour
+{
+    private void Awake()
+    {
+        GetComponent<Button>().onClick.AddListener(OpenTwitter);
+    }
+
+    private void OpenTwitter()
+    {
+        Application.OpenURL("https://twitter.com/LagrangeSpot");
+    }
+}

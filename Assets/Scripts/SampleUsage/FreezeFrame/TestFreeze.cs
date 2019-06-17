71fbd2efaad2530d8301aad7abab800a8ea338ab
ycarowr <ywrycaro@gmail.com>
2019-06-17 20:18:55 +0300

    Add test freeze

---
+++ Assets/Scripts/SampleUsage/FreezeFrame/TestFreeze.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Tools;
+using UnityEngine;
+
+public class TestFreeze : MonoBehaviour
+{
+    public FreezeFrame freezeer;
+    public float Time { get; set; }
+    public float Delay { get; set; }
+
+    public void Freeze()
+    {
+        freezeer.Freeze(Time, Delay);
+    }
+
+    public void Unfreeze()
+    {
+        freezeer.Unfreeze();
+    }
+
+    public void SetTime(string time)
+    {
+        float t;
+        float.TryParse(time, out t);
+        Time = t;
+    }
+
+    public void SetDelay(string delay)
+    {
+        float t;
+        float.TryParse(delay, out t);
+        Delay = t;
+    }
+}
--- Assets/Scripts/Tools/FreezeFrame/FreezeFrame.cs
+++ Assets/Scripts/Tools/FreezeFrame/FreezeFrame.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Patterns;
+using System.Collections;
 using UnityEngine;
 
 namespace Tools

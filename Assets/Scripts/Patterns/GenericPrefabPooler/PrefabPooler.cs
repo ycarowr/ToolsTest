b4b78957b8bc57f991d0dfabb525f891be263e85
ycarowr <ywrycaro@gmail.com>
2019-06-17 18:03:34 +0300

    Remove PrefabPool Usage Scripts

--- Assets/Scripts/Patterns/GenericPrefabPooler/PrefabPooler.cs
+++ Assets/Scripts/Patterns/GenericPrefabPooler/PrefabPooler.cs
@@ -2,59 +2,73 @@
 using Patterns;
 using UnityEngine;
 
-public class PrefabPooler : SingletonMB<PrefabPooler>
+
+namespace Patterns
 {
-    //--------------------------------------------------------------------------------------------------------------
-    
-    #region Fields
-    
-    //StatesRegister of the already pooled objects
-    private readonly Dictionary<GameObject, List<GameObject>> busyObjects =
-        new Dictionary<GameObject, List<GameObject>>();
-
-    //StatesRegister of the pooled available objects
-    private readonly Dictionary<GameObject, List<GameObject>> poolAbleObjects =
-        new Dictionary<GameObject, List<GameObject>>();
-
-    [Tooltip("How many objects will be created as soon as the game loads")] [SerializeField]
-    private readonly int startSize = 10;
-
-    [Tooltip("All pooled models have to be inside this array before the initialization")] [SerializeField]
-    private GameObject[] modelsPooled;
-
-    #endregion
-    
-    //--------------------------------------------------------------------------------------------------------------
-    
-    #region Initialization
-    
-    /// <summary>
-    ///     I am initializing it as soon as possible. You can move it to Awake or Start calls. It's up to you.
-    /// </summary>
-    private void OnEnable()
+    public interface IPooler
     {
-        //avoiding execution when the game isn't playing
-        if (!Application.isPlaying)
-            return;
-
-        //initialize the pool system
-        Initialize();
+        Dictionary<GameObject, List<GameObject>> Busy { get; }
+        Dictionary<GameObject, List<GameObject>> Available { get; }
+        GameObject[] Models { get; }
+        int StartSize { get; }
     }
-    
+
     /// <summary>
+    ///  T is the Singleton Pooler class and T1 is the Pooled.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <typeparam name="T1"></typeparam>
+    public class PrefabPooler<T, T1> : SingletonMB<T>
+        where T : class
+        where T1 : class
+    {
+        //--------------------------------------------------------------------------------------------------------------
+        
+        public Dictionary<GameObject, List<GameObject>> Busy { get; set; } = new Dictionary<GameObject, List<GameObject>>();
+        public Dictionary<GameObject, List<GameObject>> Available { get; set; } = new Dictionary<GameObject, List<GameObject>>();
+
+        [Tooltip("How many objects will be created as soon as the game loads")]
+        [SerializeField] int startSize = 10;
+        public int StartSize => startSize;
+
+        [Tooltip("All pooled models have to be inside this array before the initialization")]
+        [SerializeField] GameObject[] models;
+        public GameObject[] Models => models;
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        #region Initialization
+
+        /// <summary>
+        ///     I am initializing it as soon as possible. You can move it to Awake or Start calls. It's up to you.
+        /// </summary>
+        private void OnEnable()
+        {
+            //avoiding execution when the game isn't playing
+            if (!Application.isPlaying)
+                return;
+
+            //initialize the pool system
+            Initialize();
+        }
+
+        /// <summary>
         ///     Here is the initialization of the pooler. All the models/prefabs which you need to pool have to be inside
         ///     the modelPooled array. They will be keys for the Lists inside the pool system.
         /// </summary>
         private void Initialize()
         {
-            foreach (var model in modelsPooled)
+            if (models.Length == 0)
+                Debug.LogError("Can't pool empty objects.");
+
+            foreach (var model in models)
             {
                 //list for pool
                 var pool = new List<GameObject>();
-    
+
                 //list for busy
                 var busy = new List<GameObject>();
-    
+
                 //create the initial amount and add them to the pool
                 for (var i = 0; i < startSize; i++)
                 {
@@ -62,132 +76,142 @@ public class PrefabPooler : SingletonMB<PrefabPooler>
                     pool.Add(obj);
                     obj.SetActive(false);
                 }
-    
-                poolAbleObjects.Add(model, pool);
-                busyObjects.Add(model, busy);
+
+                Available.Add(model, pool);
+                Busy.Add(model, busy);
             }
         }
-    #endregion
-    
-    //--------------------------------------------------------------------------------------------------------------
 
-    #region Operations
-    
-    /// <summary>
-    ///     Here you can pool the prefab objects. Currently the key is a reference to the prefab that you need to get.
-    ///     Although I haven't had problems with this approach, you can come up with a solution that performs better
-    ///     using an enumeration as key.
-    /// </summary>
-    /// <param name="prefabModel"></param>
-    /// <returns></returns>
-    public GameObject Get(GameObject prefabModel)
-    {
-        GameObject pooledObj = null;
+        #endregion
 
-        if (poolAbleObjects == null)
-            Debug.LogError("Nop! PoolAble objects list is not created yet!");
+        //--------------------------------------------------------------------------------------------------------------
 
-        if (busyObjects == null)
-            Debug.LogError("Nop! Busy objects list is not created yet!");
+        #region Operations
 
-        //if prefabModel is not contained inside the StatesRegister
-        if (!poolAbleObjects.ContainsKey(prefabModel))
-            return null;
-
-        //try to grab the last element of the available objects
-        if (poolAbleObjects[prefabModel].Count > 0)
+        /// <summary>
+        ///     Here you can pool the prefab objects. Currently the key is a reference to the prefab that you need to get.
+        ///     Although I haven't had problems with this approach, you can come up with a solution that performs better
+        ///     using an enumeration as key.
+        /// </summary>
+        /// <param name="prefabModel"></param>
+        /// <returns></returns>
+        public virtual T1 Get(GameObject prefabModel)
         {
-            var size = poolAbleObjects[prefabModel].Count;
-            pooledObj = poolAbleObjects[prefabModel][size - 1];
-        }
+            GameObject pooledObj = null;
 
-        if (pooledObj != null)
-            poolAbleObjects[prefabModel].Remove(pooledObj);
-        else
-            pooledObj = Instantiate(prefabModel, transform);
+            if (Available == null)
+                Debug.LogError("Nop! PoolAble objects list is not created yet!");
 
-        //add the pooled object to the used objects list
-        busyObjects[prefabModel].Add(pooledObj);
+            if (Busy == null)
+                Debug.LogError("Nop! Busy objects list is not created yet!");
 
-        pooledObj.SetActive(true);
-        OnPool(pooledObj);
-        return pooledObj;
-    }
+            //if prefabModel is not contained inside the StatesRegister
+            if (!Available.ContainsKey(prefabModel))
+                return null;
 
-    /// <summary>
-    ///     Here you pool back objects that you no longer use. They are deactivated and
-    ///     stored back for future usage using the prefab model as key to get it back later on.
-    /// </summary>
-    /// <param name="prefabModel"></param>
-    /// <param name="pooledObj"></param>
-    public void ReleasePooledObject(GameObject prefabModel, GameObject pooledObj)
-    {
-        if (poolAbleObjects == null)
-            Debug.LogError("Nop! PoolAble objects list is not created yet!");
-
-        if (busyObjects == null)
-            Debug.LogError("Nop! Busy objects list is not created yet!");
-
-        pooledObj.SetActive(false);
-        busyObjects[prefabModel].Remove(pooledObj);
-        poolAbleObjects[prefabModel].Add(pooledObj);
-        pooledObj.transform.parent = transform;
-        pooledObj.transform.localPosition = Vector3.zero;
-        OnRelease(pooledObj);
-    }
-
-    /// <summary>
-    ///     Here you pool back objects that you no longer use. They are deactivated and
-    ///     stored back for future usage using the prefab model as key to get it back later on.
-    /// </summary>
-    /// <param name="pooledObj"></param>
-    public void ReleasePooledObject(GameObject pooledObj)
-    {
-        if (poolAbleObjects == null)
-            Debug.LogError("Nop! PoolAble objects list is not created yet!");
-
-        if (busyObjects == null)
-            Debug.LogError("Nop! Busy objects list is not created yet!");
-
-        pooledObj.SetActive(false);
-
-        foreach (var model in busyObjects.Keys)
-            if (busyObjects[model].Contains(pooledObj))
+            //try to grab the last element of the available objects
+            if (Available[prefabModel].Count > 0)
             {
-                busyObjects[model].Remove(pooledObj);
-                poolAbleObjects[model].Add(pooledObj);
+                var size = Available[prefabModel].Count;
+                pooledObj = Available[prefabModel][size - 1];
             }
 
-        pooledObj.transform.parent = transform;
-        pooledObj.transform.localPosition = Vector3.zero;
-        OnRelease(pooledObj);
+            if (pooledObj != null)
+                Available[prefabModel].Remove(pooledObj);
+            else
+                pooledObj = Instantiate(prefabModel, transform);
+
+            //add the pooled object to the used objects list
+            Busy[prefabModel].Add(pooledObj);
+
+            pooledObj.SetActive(true);
+            OnPool(pooledObj);
+
+            return pooledObj.GetComponent<T1>();
+        }
+
+        public virtual T1 Get()
+        {
+            return Get(models[0]);
+        }
+
+        /// <summary>
+        ///     Here you pool back objects that you no longer use. They are deactivated and
+        ///     stored back for future usage using the prefab model as key to get it back later on.
+        /// </summary>
+        /// <param name="prefabModel"></param>
+        /// <param name="pooledObj"></param>
+        public virtual void Release(GameObject prefabModel, GameObject pooledObj)
+        {
+            if (Available == null)
+                Debug.LogError("Nop! PoolAble objects list is not created yet!");
+
+            if (Busy == null)
+                Debug.LogError("Nop! Busy objects list is not created yet!");
+
+            pooledObj.SetActive(false);
+            Busy[prefabModel].Remove(pooledObj);
+            Available[prefabModel].Add(pooledObj);
+            pooledObj.transform.parent = transform;
+            pooledObj.transform.localPosition = Vector3.zero;
+            OnRelease(pooledObj);
+        }
+
+        /// <summary>
+        ///     Here you pool back objects that you no longer use. They are deactivated and
+        ///     stored back for future usage using the prefab model as key to get it back later on.
+        /// </summary>
+        /// <param name="pooledObj"></param>
+        public virtual void Release(GameObject pooledObj)
+        {
+            if (Available == null)
+                Debug.LogError("Nop! PoolAble objects list is not created yet!");
+
+            if (Busy == null)
+                Debug.LogError("Nop! Busy objects list is not created yet!");
+
+            pooledObj.SetActive(false);
+
+            foreach (var model in Busy.Keys)
+                if (Busy[model].Contains(pooledObj))
+                {
+                    Busy[model].Remove(pooledObj);
+                    Available[model].Add(pooledObj);
+                }
+
+            pooledObj.transform.parent = transform;
+            pooledObj.transform.localPosition = Vector3.zero;
+            OnRelease(pooledObj);
+        }
+
+        #endregion
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        #region Util
+
+        /// <summary>
+        ///     Override this method to do something when before pool an object.
+        /// </summary>
+        /// <param name="prefabModel"></param>
+        protected virtual void OnPool(GameObject prefabModel)
+        {
+            // If you need to execute some code right BEFORE the object is pooled, you can do it here.
+            // Clean references or reset variables are very common cases.
+        }
+
+        /// <summary>
+        ///     Override this method to do something after release an object.
+        /// </summary>
+        /// <param name="prefabModel"></param>
+        protected virtual void OnRelease(GameObject prefabModel)
+        {
+            // If you need to execute some code right AFTER the object is released, you can do it here.
+            // Clean references or reset variables are very common cases.
+        }
+
+        #endregion
+
+        //--------------------------------------------------------------------------------------------------------------
     }
-    
-    #endregion
-
-    //--------------------------------------------------------------------------------------------------------------
-
-    #region Events
-
-    /// <summary>
-    /// </summary>
-    /// <param name="prefabModel"></param>
-    private void OnPool(GameObject prefabModel)
-    {
-        // If you need to execute some code right BEFORE the object is pooled, you can do it here.
-        // Clean references or reset variables are very common cases.
-    }
-
-    /// <summary>
-    /// </summary>
-    /// <param name="prefabModel"></param>
-    private void OnRelease(GameObject prefabModel)
-    {
-        // If you need to execute some code right AFTER the object is released, you can do it here.
-        // Clean references or reset variables are very common cases.
-    }
-    
-    #endregion
-    
-    //--------------------------------------------------------------------------------------------------------------
 }

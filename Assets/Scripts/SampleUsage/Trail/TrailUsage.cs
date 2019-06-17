4df85771a504f7fb00cad6c7b3bed9c7271d732d
ycarowr <ywrycaro@gmail.com>
2019-06-17 21:54:57 +0300

    Add Trail animation

---
+++ Assets/Scripts/SampleUsage/Trail/TrailUsage.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Tools;
+using UnityEngine;
+
+public class TrailUsage : MonoBehaviour
+{
+    void Start()
+    {
+        var renderer = GetComponent<SpriteRenderer>();
+        var trail = GetComponentInChildren<TrailParticles>();
+        trail.PlayFromRender(renderer);
+    }
+}

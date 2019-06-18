74c1fd34b126d6c3b7f7f92e2db253808c1721ed
ycarowr <ywrycaro@gmail.com>
2019-06-18 21:31:43 +0300

    Add simple dialog system

---
+++ Assets/Scripts/Tools/DialogSystem/TextSequence/TextPiece.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Tools
+{
+    [CreateAssetMenu(menuName = "DialogSystem/TextPiece")]
+    public class TextPiece: ScriptableObject
+    {
+        public string Author;
+        [Multiline] public string Text;
+        public DialogSystem.DialogAutoAction OnPressNext = DialogSystem.DialogAutoAction.Next;
+        public UnityEvent OnNext = new UnityEvent();
+    }
+}

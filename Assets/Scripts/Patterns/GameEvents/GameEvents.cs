d34587b63a795daca5d60f5380b73011a6a9d84b
ycarowr <ywrycaro@gmail.com>
2019-06-18 23:21:16 +0300

    Add generic game events

---
+++ Assets/Scripts/Patterns/GameEvents/GameEvents.cs
@@ -0,0 +1,35 @@
+﻿using Patterns;
+
+namespace Patterns.GameEvents
+{
+    //----------------------------------------------------------------------------------------------------------
+
+    public class GameEvents : Observer<GameEvents>
+    {
+        
+    }
+
+    //----------------------------------------------------------------------------------------------------------
+
+    #region Game Events Definition
+
+    /// <summary>
+    ///      Broadcast of the event to the Listeners.
+    /// </summary>
+    public interface ISampleEvent1 : ISubject
+    {
+        void OnISampleEvent1();
+    }
+
+    /// <summary>
+    ///      Broadcast of the event to the Listeners.
+    /// </summary>
+    public interface ISampleEvent2 : ISubject
+    {
+        void OnISampleEvent2();
+    }
+
+    #endregion
+
+    //----------------------------------------------------------------------------------------------------------
+}

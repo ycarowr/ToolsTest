b2305ce17b5824ad774561952e7912e0c0f0934a
ycarowr <ywrycaro@gmail.com>
2019-07-03 22:01:45 +0300

    Rework dialog example

---
+++ Assets/Scripts/SampleUsage/DialogSystem/TestDialog.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Tools;
+using UnityEngine;
+
+namespace TestDialog
+{
+    public class TestDialog : MonoBehaviour
+    {
+        public TextSequence TestSequence;
+        public DialogSystem Dialog;
+
+        [Button]
+        public void Show()
+        {
+            Dialog.Show();
+        }
+        
+        [Button]
+        public void Hide()
+        {
+            Dialog.Hide();
+        }
+        
+        [Button]
+        public void Write()
+        {
+            Dialog.Write(TestSequence);
+        }
+    }
+}
